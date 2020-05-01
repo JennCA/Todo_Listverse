474caca80c50a1e0b88a3cd53bc62012363a2600
JennCA <jennifer.cicvarova@gmail.com>
2020-05-01 08:20:07 +0200

    second commit

--- todo-app/src/app/components/todo-list/todo-list.component.ts
+++ todo-app/src/app/components/todo-list/todo-list.component.ts
@@ -6,21 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo-list.component.css']
 })
 export class TodoListComponent implements OnInit {
-  todolist = JSON.parse(localStorage.getItem('todo')) || [];
+  todos = JSON.parse(localStorage.getItem('todo')) || [];
   indexToRemove = 1;
 
   addTodo(todoValue: string) {
     if (todoValue) {
-      this.todolist.push(todoValue);
-      localStorage.setItem('todo', JSON.stringify(this.todolist));
+      this.todos.push(todoValue);
+      localStorage.setItem('todo', JSON.stringify(this.todos));
     }
     // console.log(this.todolist);
   }
 
   removeTodo(todoValue: number) : void {
-    this.todolist.splice(todoValue, 1);
-    this.todolist.slice(this.indexToRemove, 1);
-    localStorage.setItem('todo', JSON.stringify(this.todolist));
+    this.todos.splice(todoValue, 1);
+    this.todos.slice(this.indexToRemove, 1);
+    localStorage.setItem('todo', JSON.stringify(this.todos));
     // console.log(this.todolist);
     // console.log(localStorage);
   }
